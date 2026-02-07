59611eb8c3b96c24e75d2ad7ec2c2499452a66f3
kali <xaviernavarro2017@gmail.com>
2026-02-07 13:02:25 -0300

    Effects

--- src/sections/projects.tsx
+++ src/sections/projects.tsx
@@ -4,6 +4,7 @@ import { StyledText, StyledTittle, Subtittle } from "../components/texts"
 import { palette } from "../common/styles"
 import { projectsList } from "../common/projectsList"
 import { ProjectCard } from "../components/projectCard"
+import { useObserver } from "../common/observer"
 
 const Line = styled.div`
     background-color: ${palette.yellow};
@@ -14,6 +15,7 @@ const Line = styled.div`
 const MyPhoto = styled.img`
     height: 12rem;
     width: 12rem;
+    border-radius: 100%;
 `
 
 const InfoContainer = styled.div`
@@ -23,16 +25,40 @@ const InfoContainer = styled.div`
     margin-bottom: 10rem;
 `
 
-const ProjectsContainer = styled.div`
+const ProjectsContainer = styled.div<{ $visible: boolean }>`
     display: flex;
     gap: 2rem;
+
+    & > * {
+        opacity: 0;
+        transform: translateY(40px);
+        transition:
+            opacity 0.6s ease,
+            transform 0.6s ease;
+    }
+
+    ${({$visible}) => 
+        $visible && `
+            & > * {
+                opacity: 1;
+                transform: translateY(0);
+            }
+            
+            & > *:nth-child(1) {transition-delay: 0.1s;}
+            & > *:nth-child(2) {transition-delay: 0.25s;}
+            & > *:nth-child(3) {transition-delay: 0.4s;}
+        `
+    }
+    
 `
 
+
 export const Projects = () => {
+    const {ref, visible} = useObserver();
     return (
         <Section>
             <InfoContainer>
-                <MyPhoto src="/github.png" />
+                <MyPhoto src="/Photo.jpeg" />
                 <div>
                     <Subtittle>Web Developer</Subtittle>
                     <Line />
@@ -42,7 +68,7 @@ export const Projects = () => {
             </InfoContainer>
 
             <StyledText>Main Projects:</StyledText>
-            <ProjectsContainer>
+            <ProjectsContainer ref={ref} $visible={visible}>
                 {
                   projectsList.map((e, i)=> {
                     return(
--- src/sections/stack.tsx
+++ src/sections/stack.tsx
@@ -2,6 +2,7 @@ import styled from "styled-components"
 import { CentralSection } from "../components/section"
 import { StyledTittle } from "../components/texts"
 import { TechList } from "../common/techlist"
+import { useObserver } from "../common/observer"
 
 const TechImg = styled.img`
     height: 4rem;
@@ -15,19 +16,51 @@ const TechImg = styled.img`
     };
 `
 
+const TechContainer = styled.div<{ $visible: boolean }>`
+
+
+    & > * {
+        opacity: 0;
+        transform: translateY(40px);
+        transition:
+            opacity 0.6s ease,
+            transform 0.6s ease;
+    }
+
+    ${({ $visible }) =>
+        $visible && `
+            & > * {
+                opacity: 1;
+                transform: translateY(0);
+            }
+            
+            & > *:nth-child(1) {transition-delay: 0.1s;}
+            & > *:nth-child(2) {transition-delay: 0.2s;}
+            & > *:nth-child(3) {transition-delay: 0.3s;}            
+            & > *:nth-child(4) {transition-delay: 0.4s;}
+            & > *:nth-child(5) {transition-delay: 0.5s;}
+            & > *:nth-child(6) {transition-delay: 0.6s;}
+            & > *:nth-child(7) {transition-delay: 0.7s;}
+            & > *:nth-child(8) {transition-delay: 0.8s;}
+        `
+    }
+`
+
 export const Stack = () => {
+    const { ref, visible } = useObserver();
+
     return (
         <CentralSection>
             <StyledTittle>Stack</StyledTittle>
-            <div>
+            <TechContainer ref={ref} $visible={visible}>
                 {
-                    TechList.map((e, i)=> {
+                    TechList.map((e, i) => {
                         return (
-                            <TechImg src={e.link} key={i}/>
+                            <TechImg src={e.link} key={i} />
                         )
                     })
                 }
-            </div>
+            </TechContainer>
         </CentralSection>
     )
 }

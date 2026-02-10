03cce0e3b2f9e5f31236d24d24a2011c437e7bb1
kali <xaviernavarro2017@gmail.com>
2026-02-10 09:10:20 -0300

    little fix

--- src/common/projectsList.ts
+++ src/common/projectsList.ts
@@ -5,11 +5,6 @@ export const projectsList = [
         link: 'https://ufc-bets.vercel.app/',
     },
     {
-        name: 'Enfy',
-        des: '[Currently on Working] Enfy is designed to help companies manage events and ensure attendance.',
-        link: ''
-    },
-    {
         name: 'PNL Calendar',
         des: 'Tired of dont know how much you earn pastday? use PNL Calendar to have a better tracking',
         link: 'https://pnl-calendar-green.vercel.app/',
